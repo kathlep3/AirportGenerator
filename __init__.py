40f9c0d4f804621bc0045aa2d76a0d2348ae0314
kathlep3 <166050399+kathlep3@users.noreply.github.com>
2024-07-09 09:51:46 -0700

    Add files via upload
    Uploaded engine, events, views packages. project2.py is the main module of coding to look at, the packages are provided.

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# p2app/views/__init__.py
+#
+# ICS 33 Spring 2024
+# Project 2: Learning to Fly
+#
+# Initialization module for the p2app.views package.
+#
+# YOU WILL NOT NEED TO MODIFY THIS FILE AT ALL
+
+from .main import MainView
